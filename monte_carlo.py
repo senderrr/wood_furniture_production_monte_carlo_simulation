09dfc9c4e18eb0cd8bc4c06358f50be588498def
Alex Wieker <wiekerr@gmail.com>
2019-04-21 18:46:39 -0500

    working on priority queue

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,4 +1,5 @@
 import numpy as np
+import queue
 
 def pert(low, likely, high, confidence=4, samples=10000):
     """I got this function from Instructor Weible's Lecture Notes"""
@@ -20,66 +21,127 @@ def pert(low, likely, high, confidence=4, samples=10000):
 # miller & ranom
 
 # what to do about my rounding?
-def orders(low, likely, high, order_count_confidence, order_confidence, samples):
+def orders(low, likely, high, daily_count_confidence, order_size_confidence, samples):
     item_order_options = ['A', 'B', 'C', 'D', 'E']
+
+    all_items = []
+
+    # time_counter = 0
+    # while time_counter <= 8:
+    #     time_counter += j + 1
+
     for i in range(5):
-        daily_order_count = pert(low, likely, high, confidence=order_count_confidence, samples=samples)
+        print(i + 1)
+        daily_order_count = pert(low, likely, high, confidence=daily_count_confidence, samples=samples)
         daily_order_count = np.around(daily_order_count).astype(int)
-
-        all_items = []
-
+        print('daily order count:', daily_order_count)
+        # L = queue.Queue(maxsize=daily_order_count)
+        # for z in range(daily_order_count[0])
+        # daily order_count = z
+        # for q in range(daily_order_count[0])
+        #     daily_order_count = pert(low, likely, high, confidence=order_count_confidence, samples=samples)
         for j in range(daily_order_count[0]):
-            order_size = pert(1, 3, 10, confidence=order_confidence, samples=samples)
+            order_size = pert(1, 3, 10, confidence=order_size_confidence, samples=samples)
             order_size = np.around(order_size).astype(int)
-            #print(order_size)
             order_items = np.random.choice(item_order_options, size=order_size, p=[0.1, 0.2, 0.4, 0.2, 0.1]) # put in probablity
+            print(order_items, 'day:', i + 1, 'order:', j + 1)
             all_items.append(order_items)
-            #print(order_items)
+# 6, 5, 5, 7, 4
+        #print(order_items)
+        # L.put(order_items)
+        # t = L.get()
+        # print(t)
 
-        return all_items
+        #print(order_items)
+
+    return all_items
+
+#def priority():
+
+def get_que(Q, build_confidnece, samples):
+    # daily_que  = queue.Queue(maxsize=20)
+    # work = True
+    # while work is True:
+    #     time_counter = 0
+    #
+    time_counter = 0
+    while time_counter <= 7.5:
+        a_counter = 0
+        b_counter = 0
+        c_counter = 0
+        d_counter = 0
+        e_counter = 0
+
+        for t in Q:
+            #print(t)
+            items_A = np.count_nonzero(t == 'A')
+            a_counter += items_A
+            #print('max',np.max(a_counter))
+            # A_list.append(items_A)
+            #print(np.count_nonzero(items_A))
+            items_B = np.count_nonzero(t == 'B')
+            b_counter += items_B
+
+            items_C = np.count_nonzero(t == 'C')
+            c_counter += items_C
+
+            items_D = np.count_nonzero(t == 'D')
+            d_counter += items_D
+
+            items_E = np.count_nonzero(t == 'E')
+            e_counter += items_E
 
 
-def Queue(Q):
 
-    for w in Q:
-        print(w)
-        order_A = np.count_nonzero(w == 'A')
-        order_B = np.count_nonzero(w == 'B')
-        order_C = np.count_nonzero(w == 'C')
-        order_D = np.count_nonzero(w == 'D')
-        order_E = np.count_nonzero(w == 'E')
+        order_A_time_range = pert(7, 10, 15, confidence=build_confidnece, samples=samples)
+        order_A_time_range = np.around(order_A_time_range, decimals=2)
+
+        order_B_time_range = pert(1, 2, 5, confidence=build_confidnece, samples=samples)
+        order_B_time_range = np.around(order_B_time_range, decimals=2)
+
+        order_C_time_range= pert(6, 8, 10, confidence=build_confidnece, samples=samples)
+        order_C_time_range = np.around(order_C_time_range, decimals=2)
+
+        order_D_time_range = pert(5, 9, 13, confidence=build_confidnece, samples=samples)
+        order_D_time_range = np.around(order_D_time_range, decimals=2)
 
 
-        order_A_time = order_A * 10
-        order_B_time = order_B * 3
-        order_C_time = order_C * 8
-        order_D_time = order_D * 9
-        order_E_time = order_E * 15
+        order_E_time_range = pert(12, 15, 20, confidence=build_confidnece, samples=samples)
+        order_E_time_range = np.around(order_E_time_range, decimals=2)
 
-        print('Order A: hours to build:', order_A_time)
-        print('Order B: hours to build:', order_B_time)
-        print('Order C: hours to build:', order_C_time)
-        print('Order D: hours to build:', order_D_time)
-        print('Order E: hours to build:', order_E_time)
-        print('All Orders : hours to build:', (order_A_time + order_B_time + order_C_time + order_D_time + order_E_time))
+        order_A_time = a_counter * order_A_time_range
+        #print(order_A_time)
+        order_B_time = b_counter * order_B_time_range
+        order_C_time = c_counter * order_C_time_range
+        order_D_time = d_counter * order_D_time_range
+        order_E_time = e_counter * order_E_time_range
+        if order_A_time != 0:
+            time_counter += order_A_time
+        elif order_B_time != 0:
+            time_counter += order_B_time
+        elif order_C_time != 0:
+            time_counter += order_C_time
+        elif order_D_time != 0:
+            time_counter += order_D_time
+        elif order_E_time != 0:
+            time_counter += order_E_time
+        else:
+            #print(time_counter)
+            #print('days to build:', time_counter/7.5)
+            break
 
-week_simulation = orders(0, 5, 10, order_count_confidence=4, order_confidence=2, samples=1)
 
-Queue(week_simulation)
+        #print(np.count_nonzero(items_A))
 
-# production  occupations: 3.0 per week
-# https://www.bls.gov/cps/cpsaat47.htm#cps_eeann_abs_ft_occu_ind.f.1
+            # print('Order A: hours to build:', order_A_time)
+            # print('Order B: hours to build:', order_B_time)
+            # print('Order C: hours to build:', order_C_time)
+            # print('Order D: hours to build:', order_D_time)
+            # print('Order E: hours to build:', order_E_time)
+            # print('All Orders : hours to build:', (order_A_time + order_B_time + order_C_time + order_D_time + order_E_time))
 
-# generate how many workers are available
-# def worker_count(low, likely, high, n_workers, samples):
-#     worker_range = pert(low, likely, high, samples=samples)
-#
-#     total_weekly_hours_missed = max(worker_range)
-#     hour_missed_range = round(total_weekly_hours_missed, 0)
-#     print(hour_missed_range)
-#
-#     weekly_hours_missed = np.random.randint(hour_missed_range, size=n_workers)
-#     print(weekly_hours_missed)
-#
-#     total_worker_hours = (n_workers * 35) - np.sum(weekly_hours_missed)
-#     print(total_worker_hours)
+
+
+#def time():
+
+get_que(orders(1, 5, 10, daily_count_confidence=4, order_size_confidence=2, samples=1), build_confidnece=2, samples=1)
