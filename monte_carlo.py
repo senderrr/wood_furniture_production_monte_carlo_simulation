51cdf573f13f110357d944869fa2d0aabd9c2ac1
Alex Wieker <wiekerr@gmail.com>
2019-04-21 22:19:48 -0500

    working on first come queue

--- monte_carlo.py
+++ monte_carlo.py
@@ -26,44 +26,133 @@ def orders(low, likely, high, daily_count_confidence, order_size_confidence, sam
 
     all_items = []
 
-    # time_counter = 0
-    # while time_counter <= 8:
-    #     time_counter += j + 1
-
     for i in range(5):
-        print(i + 1)
         daily_order_count = pert(low, likely, high, confidence=daily_count_confidence, samples=samples)
         daily_order_count = np.around(daily_order_count).astype(int)
         print('daily order count:', daily_order_count)
-        # L = queue.Queue(maxsize=daily_order_count)
-        # for z in range(daily_order_count[0])
-        # daily order_count = z
-        # for q in range(daily_order_count[0])
-        #     daily_order_count = pert(low, likely, high, confidence=order_count_confidence, samples=samples)
+
         for j in range(daily_order_count[0]):
-            order_size = pert(1, 3, 10, confidence=order_size_confidence, samples=samples)
+            order_size = pert(1, 2, 15, confidence=order_size_confidence, samples=samples)
             order_size = np.around(order_size).astype(int)
-            order_items = np.random.choice(item_order_options, size=order_size, p=[0.1, 0.2, 0.4, 0.2, 0.1]) # put in probablity
+            order_items = np.random.choice(item_order_options, size=order_size, p=[0.1, 0.2, 0.4, 0.2, 0.1])
             print(order_items, 'day:', i + 1, 'order:', j + 1)
             all_items.append(order_items)
-# 6, 5, 5, 7, 4
-        #print(order_items)
-        # L.put(order_items)
-        # t = L.get()
-        # print(t)
 
-        #print(order_items)
+    print('---')
 
     return all_items
 
-#def priority():
 
-def get_que(Q, build_confidnece, samples):
+def first_come_queue(q, build_confidence, samples):
+    time_counter = 0
+    while time_counter <= 7.5:
+        order_counter = 0
+
+        for fcq in q:
+            order_counter += 1
+            a_count = fcq[fcq == 'A']
+            a_count = np.count_nonzero(a_count)
+
+            b_count = fcq[fcq == 'B']
+            b_count = np.count_nonzero(b_count)
+
+            c_count = fcq[fcq == 'C']
+            c_count = np.count_nonzero(c_count)
+
+            d_count = fcq[fcq == 'D']
+            d_count = np.count_nonzero(d_count)
+
+            e_count = fcq[fcq == 'E']
+            e_count = np.count_nonzero(e_count)
+
+            time_list = []
+            machine_time_swap = .25
+            time_counter = 0
+
+            for a in range(a_count):
+                order_a_time = pert(4, 8, 11, confidence=build_confidence, samples=samples)
+                order_a_time = order_a_time + machine_time_swap
+                order_a_time = np.around(order_a_time, decimals=1)
+
+                time_list.append(order_a_time)
+                time_counter += order_a_time
+                # if time_counter >= 7.5:
+                #     print(time_counter)
+                #     print('Work day is over')
+                #     continue
+
+            for b in range(b_count):
+                order_b_time = pert(1, 2, 5, confidence=build_confidence, samples=samples)
+                order_b_time = order_b_time + machine_time_swap
+                order_b_time = np.around(order_b_time, decimals=1)
+                time_list.append(order_b_time)
+                time_counter += order_b_time
+                # if time_counter >= 7.5:
+                #     print(time_counter)
+                #     print('Work day is over')
+                #     continue
+
+            for c in range(c_count):
+                order_c_time = pert(.5, 1, 3, confidence=build_confidence, samples=samples)
+                order_c_time = order_c_time + machine_time_swap
+                order_c_time = np.around(order_c_time, decimals=1)
+                time_list.append(order_c_time)
+                time_counter += order_c_time
+                # if time_counter >= 7.5:
+                #     print(time_counter)
+                #     print('Work day is over')
+                #     continue
+
+            for d in range(d_count):
+                order_d_time = pert(2, 3, 6, confidence=build_confidence, samples=samples)
+                order_d_time = order_d_time + machine_time_swap
+                order_d_time = np.around(order_d_time, decimals=1)
+                time_list.append(order_d_time)
+                time_counter += order_d_time
+                # if time_counter >= 7.5:
+                #     print(time_counter)
+                #     print('Work day is over')
+                #     continue
+
+            for e in range(e_count):
+                order_e_time = pert(5, 7, 10, confidence=build_confidence, samples=samples)
+                order_e_time = order_e_time + machine_time_swap
+                order_e_time = np.around(order_e_time, decimals=1)
+                time_list.append(order_e_time)
+                time_counter += order_e_time
+                # if time_counter >= 7.5:
+                #     print(time_counter)
+                #     print('Work day is over')
+                #     continue
+
+
+            all_item_build_time = np.sum(time_list)
+            all_item_build_time = np.around(all_item_build_time, decimals=1)
+            time_to_make_order = all_item_build_time  # + machine_swap_time
+            time_to_make_order = np.around(time_to_make_order, decimals=1)
+            print('Order', order_counter, ':', fcq, '---', 'Time to make order:', time_to_make_order, 'hours')
+            if time_counter >= 7.5:
+                print(time_counter[0])
+                print('Work day is over.')
+                continue
+
+
+
+
+
+
+
+
+
+
+
+def priority_queue(Q, build_confidence, samples):
     # daily_que  = queue.Queue(maxsize=20)
     # work = True
     # while work is True:
     #     time_counter = 0
     #
+    week_time_counter = []
     time_counter = 0
     while time_counter <= 7.5:
         a_counter = 0
@@ -72,49 +161,48 @@ def get_que(Q, build_confidnece, samples):
         d_counter = 0
         e_counter = 0
 
-        for t in Q:
+        for pq in Q:
             #print(t)
-            items_A = np.count_nonzero(t == 'A')
+            items_A = np.count_nonzero(pq == 'A')
             a_counter += items_A
             #print('max',np.max(a_counter))
             # A_list.append(items_A)
             #print(np.count_nonzero(items_A))
-            items_B = np.count_nonzero(t == 'B')
+            items_B = np.count_nonzero(pq == 'B')
             b_counter += items_B
 
-            items_C = np.count_nonzero(t == 'C')
+            items_C = np.count_nonzero(pq == 'C')
             c_counter += items_C
 
-            items_D = np.count_nonzero(t == 'D')
+            items_D = np.count_nonzero(pq == 'D')
             d_counter += items_D
 
-            items_E = np.count_nonzero(t == 'E')
+            items_E = np.count_nonzero(pq == 'E')
             e_counter += items_E
 
 
-
-        order_A_time_range = pert(7, 10, 15, confidence=build_confidnece, samples=samples)
+        order_A_time_range = pert(7, 10, 15, confidence=build_confidence, samples=samples)
         order_A_time_range = np.around(order_A_time_range, decimals=2)
 
-        order_B_time_range = pert(1, 2, 5, confidence=build_confidnece, samples=samples)
+        order_B_time_range = pert(1, 2, 5, confidence=build_confidence, samples=samples)
         order_B_time_range = np.around(order_B_time_range, decimals=2)
 
-        order_C_time_range= pert(6, 8, 10, confidence=build_confidnece, samples=samples)
+        order_C_time_range= pert(6, 8, 10, confidence=build_confidence, samples=samples)
         order_C_time_range = np.around(order_C_time_range, decimals=2)
 
-        order_D_time_range = pert(5, 9, 13, confidence=build_confidnece, samples=samples)
+        order_D_time_range = pert(5, 9, 13, confidence=build_confidence, samples=samples)
         order_D_time_range = np.around(order_D_time_range, decimals=2)
 
 
-        order_E_time_range = pert(12, 15, 20, confidence=build_confidnece, samples=samples)
+        order_E_time_range = pert(12, 15, 20, confidence=build_confidence, samples=samples)
         order_E_time_range = np.around(order_E_time_range, decimals=2)
 
         order_A_time = a_counter * order_A_time_range
-        #print(order_A_time)
         order_B_time = b_counter * order_B_time_range
         order_C_time = c_counter * order_C_time_range
         order_D_time = d_counter * order_D_time_range
         order_E_time = e_counter * order_E_time_range
+
         if order_A_time != 0:
             time_counter += order_A_time
         elif order_B_time != 0:
@@ -126,9 +214,12 @@ def get_que(Q, build_confidnece, samples):
         elif order_E_time != 0:
             time_counter += order_E_time
         else:
+            time_counter.append(week_time_counter)
+            print(week_time_counter)
             #print(time_counter)
             #print('days to build:', time_counter/7.5)
-            break
+            continue
+    print(week_time_counter)
 
 
         #print(np.count_nonzero(items_A))
@@ -144,4 +235,5 @@ def get_que(Q, build_confidnece, samples):
 
 #def time():
 
-get_que(orders(1, 5, 10, daily_count_confidence=4, order_size_confidence=2, samples=1), build_confidnece=2, samples=1)
+first_come_queue(orders(0, 2, 10, daily_count_confidence=4, order_size_confidence=4, samples=1), build_confidence=4, samples=1)
+#priority_queue(orders(0, 2, 10, daily_count_confidence=4, order_size_confidence=4, samples=1), build_confidnece=4, samples=1)
