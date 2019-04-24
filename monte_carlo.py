25fe7617fe171c23377e61caee44e860b622a7cc
Alex Wieker <wiekerr@gmail.com>
2019-04-24 13:46:09 -0500

    Added wait time to first come queue function; Working on Priority Queue now.

--- monte_carlo.py
+++ monte_carlo.py
@@ -67,26 +67,12 @@ def orders(low, likely, high, daily_count_confidence, order_size_confidence, sam
 
     print('---')
     order_len = list(range(len(all_orders)))
-    df = pd.DataFrame({'Order #': order_len,
-                    'Day': column_day,
-                    'Item A': column_a,
-                    'Item B': column_b,
-                    'Item C': column_c,
-                    'Item D': column_d,
-                    'Item E': column_e})
+    df = pd.DataFrame({'Order #': order_len, 'Day': column_day, 'Item A': column_a, 'Item B': column_b,
+                       'Item C': column_c, 'Item D': column_d, 'Item E': column_e})
     df['Order #'] = df['Order #'] + 1
 
     print(df)
     return df
-    # dictionary version
-    # start = z[0] + 1
-    # stop = z[-1] + 1
-    # order_id = range(start,stop)
-    # for fcq in all_orders:
-    #     t = fcq.tolist()
-    #     all_orders.append(t)
-    # res = dict(zip(order_id, all_orders))
-    # print(res)
 
 
 def fcq_hours(array, machine_time_swap, low, likely, high, confidence):
@@ -140,63 +126,24 @@ def first_come_queue(df, build_confidence):
     df['Build Time'] = df['A T'] + df['B T'] + df['C T'] + df['D T'] + df['E T']
     df['Pick Up Day'] = df['Build Time'].cumsum(axis=0)
     df['Pick Up Day'] = df['Pick Up Day'] + 1
-    df = df[['Order #', 'Day', 'Item A', 'Item C', 'Item D', 'Item E',
-             'Build Time', 'Pick Up Day']]
+    df['Wait Time'] = df['Pick Up Day'] - df['Day']
+    df = df[['Order #', 'Day', 'Item A', 'Item B', 'Item C', 'Item D', 'Item E',
+             'Build Time', 'Pick Up Day', 'Wait Time']]
 
-    # pd.set_option('display.max_columns', 15)
-
-    # print(q)decade_df['Year'] = decade_df['Year'].str[:3]
     print(df)
 
+# columns for keeping track of whats made or not; wait time statistics
+def priority_queue(pq_df, build_confidence, samples):
+    print(pq_df)
 
-def priority_queue(Q, build_confidence, samples):
-    week_time_counter = []
-    time_counter = 0
-    while time_counter <= 7.5:
-        a_counter = 0
-
-        for pq in Q:
-            #print(t)
-            items_A = np.count_nonzero(pq == 'A')
-            a_counter += items_A
-
-        order_A_time_range = pert(7, 10, 15, confidence=build_confidence, samples=samples)
-        order_A_time_range = np.around(order_A_time_range, decimals=2)
-        order_A_time = a_counter * order_A_time_range
+    pq_df = pq_df.groupby('Day')['Item A', 'Item B', 'Item C', 'Item D', 'Item E'].sum()
+    pq_df['Most Ordered Item'] = pq_df.max(axis=1)
+    print(pq_df)
 
 
 generate_orders = orders(0, 2, 10, daily_count_confidence=4, order_size_confidence=4, samples=1)
-first_come_queue(generate_orders, build_confidence=4)
+#first_come_queue(generate_orders, build_confidence=4)
 
-# priority_queue(generate_orders), build_confidence=4, samples=1)
+priority_queue(generate_orders, build_confidence=4, samples=1)
 
-# old code
-       # print(q)
-# def first_come_queue(q, build_confidence, samples):
 
-# order_counter = 0
-
-    # for fcq in q:
-    #     order_counter += 1
-    #     a_count = fcq[fcq == 'A']
-    #     a_count = np.count_nonzero(a_count)
-    #
-    #     time_list = []
-    #     machine_time_swap = 0.5
-    #
-    #     for a in range(a_count):
-    #         order_a_time = pert(4, 8, 11, confidence=build_confidence, samples=samples)
-    #         order_a_time = order_a_time/7.5
-    #         order_a_time = order_a_time + machine_time_swap
-    #         order_a_time = np.around(order_a_time, decimals=1)
-    #
-    #         time_list.append(order_a_time)
-    #
-    #
-    #     all_item_build_time = np.sum(time_list)
-    #     all_item_build_time = np.around(all_item_build_time, decimals=1)
-    #     time_to_make_order = all_item_build_time  # + machine_swap_time
-    #     time_to_make_order = np.around(time_to_make_order, decimals=1)
-    #     time_to_make_order = str(time_to_make_order)
-    #     print('Order', order_counter, ':', fcq, '---', 'Time to make Order. \ +
-    #   Days:', time_to_make_order[0], 'Hours:', time_to_make_order[2], time_to_make_order)
